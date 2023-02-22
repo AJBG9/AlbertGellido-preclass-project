f73ae62e781617b1c35979060dee8d5938e8b8bd
AJBG9 <toothlezz09@gmail.com>
2023-02-22 16:05:41 +0800

    activity_4: return array of ages that you could only see one time

--- activities/activity_4.rb
+++ activities/activity_4.rb
@@ -2,3 +2,23 @@
 #  Write a function that takes an array of ages.
 #  The function will return an Array of ages that you could only see one time.
 #  Print on console the result.
+
+def arr_ages(ages)
+  time = []
+  bool = false
+  ages.each_with_index do |val, index|
+    ages.each_with_index do |val_2, index_1|
+      if val == ages[index_1] && index != index_1
+        bool = false
+        break
+      else
+        bool = true
+      end
+    end
+    if bool
+      time << val
+    end
+  end
+  return time
+end
+p arr_ages([1,1,7,9,10,11,10])
