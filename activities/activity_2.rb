457631367b9791ca2fa3dbd00fa1ecec33ed5adb
AJBG9 <toothlezz09@gmail.com>
2023-02-22 15:15:45 +0800

    activity_2: takes hash and return full name

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,13 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+#
+
+def fname(name)
+  ans = ""
+  name.each do |key, value |
+    ans = ans + value + " "
+  end
+  return ans
+end
+p fname(Hash[first_name: "Albert Jeremy Bernard", middle_name: "Pena", last_name: "Gellido"])
